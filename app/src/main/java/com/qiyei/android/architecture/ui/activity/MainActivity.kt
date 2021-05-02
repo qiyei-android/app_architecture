78be0a67576b57d94363c43aea4c544aea8e1c8e
qiyei2015 <1273482124@qq.com>
2021-05-02 19:15:39 +0800

    project:初始化工程

---
+++ app/src/main/java/com/qiyei/android/architecture/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.qiyei.android.architecture.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qiyei.android.architecture.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
