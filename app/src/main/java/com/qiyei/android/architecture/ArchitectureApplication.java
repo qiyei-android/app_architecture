d086b90221549321e4a41168a7a62fd6ec3cf144
qiyei2015 <1273482124@qq.com>
2021-05-02 20:15:40 +0800

    project:引入Hilt

---
+++ app/src/main/java/com/qiyei/android/architecture/ArchitectureApplication.java
@@ -0,0 +1,9 @@
+package com.qiyei.android.architecture;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ArchitectureApplication extends Application {
+}
