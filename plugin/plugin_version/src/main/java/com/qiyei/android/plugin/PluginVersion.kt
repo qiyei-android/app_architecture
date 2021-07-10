81996ef2925a609e48d7f06ec6a249285cb114b5
qiyei2015 <xiangwei.han@faw-vw.com>
2021-07-10 16:35:46 +0800

    Hilt:解决IDE报错问题

--- plugin/plugin_version/src/main/java/com/qiyei/android/plugin/PluginVersion.kt
+++ plugin/plugin_version/src/main/java/com/qiyei/android/plugin/PluginVersion.kt
@@ -4,9 +4,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 
-/**
- * 如果出现红色警告可以忽略，不会影响项目的编译和运行
- */
 class PluginVersion : Plugin<Project> {
     override fun apply(project: Project) {
     }
